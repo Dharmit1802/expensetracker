6c38cb02868142b7e9e06b60eefdb41ea32cbe60
Dharmit1802 <dharmit1802@gmail.com>
2024-03-04 19:18:42 +0530

    d2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -26,6 +26,25 @@ export default function Dashboard() {
   const [showaddexpense, SetShowExpense] = useState(false);
   const [expenses, setExpenses] = useState(data);
 
+  const [formData, setFormData] = useState({
+   title : "",
+   amount : "",
+   category : "",
+   date : ""
+  });
+
+  console.log(formData);
+
+  function handleChange(event) {
+    const { name, value, checked, type } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  console.log(formData);
+
   const CATEGORIES = [
     { name: 'technology', color: '#3b82f6' },
     { name: 'science', color: '#16a34a' },
@@ -44,6 +63,10 @@ export default function Dashboard() {
     showaddexpense ? SetShowExpense(false) : SetShowExpense(true);
   }
 
+  function handlesubmit(e){
+    e.preventDefault();
+  }
+
   return (
     <div className='w-[100%] flex flex-col h-[100vh]'>
       <div className='text-white bg-black sticky top-0 w-[90%] mx-auto px-[70px] py-5 flex justify-between items-center'>
@@ -60,23 +83,23 @@ export default function Dashboard() {
         <button onClick={handleaddexpense} className="bg-red-500 text-black px-5 py-2 font-semibold rounded-full text-[21px]">Add expense</button>
       </div>
 
-      <form className={`w-[85%] ${showaddexpense ? "block" : "hidden"} text-xl gap-y-3 rounded-3xl bg-gray-700 mx-auto flex flex-col justify-between  px-[70px] py-5 text-white`} >
+      <form onSubmit={handlesubmit} className={`w-[85%] ${showaddexpense ? "block" : "hidden"} text-xl gap-y-3 rounded-3xl bg-gray-700 mx-auto flex flex-col justify-between  px-[70px] py-5 text-white`} >
         <div className='flex w-full flex-col md:gap-y-0 gap-y-3 md:flex-row md:gap-x-[120px]'> 
           <div className='flex gap-2 items-center'>
             <label for="title" className="capitalize">title:</label>
-            <input id='title' type="text" maxlength="50" placeholder="title here" className='rounded-full border-gray-900 text-black outline-1 px-4 py-2 w-full md:w-[35rem] bg-gray-300'/>
+            <input id='title' name="title" value={formData.title} type="text" maxlength="50" placeholder="title here" onChange={handleChange} className='rounded-full border-gray-900 text-black outline-1 px-4 py-2 w-full md:w-[35rem] bg-gray-300'/>
           </div>
 
           <div className='flex gap-2 items-center'>
             <label for="amount" className="capitalize">amount:</label>
-            <input id='amount' type="text" maxlength="15" placeholder="amount here" className='rounded-full border-gray-900 text-black outline-1 px-4 py-2 w-full md:w-[20rem] bg-gray-300'/>
+            <input id='amount' name="amount" value={formData.amount} type="text" maxlength="15" placeholder="amount here" onChange={handleChange} className='rounded-full border-gray-900 text-black outline-1 px-4 py-2 w-full md:w-[20rem] bg-gray-300'/>
           </div>
         </div>
 
         <div className='flex w-full flex-col md:gap-y-0 gap-y-3 md:flex-row md:gap-x-[430px]'> 
           <div className='flex gap-2 items-center'>
             <label for="category" className="capitalize">category:</label>
-            <select id='category' className="rounded-full px-4 py-2 flex items-center bg-gray-300 text-gray-700">
+            <select id='category' onChange={handleChange} name="category" value={formData.category} className="rounded-full px-4 py-2 flex items-center bg-gray-300 text-gray-700">
               <option value="">Choose category:</option>
               {CATEGORIES.map((cat) => (<option key={cat.name} value={cat.name}> {cat.name.toUpperCase()}</option>))}
 
@@ -84,8 +107,8 @@ export default function Dashboard() {
           </div>
 
           <div className='flex gap-2 items-center'>
-            <label for="category" className="capitalize">date:</label>
-            <input type='date' max={today}  className="rounded-full px-4 py-2 flex items-center bg-gray-300 text-gray-400"></input>
+            <label for="date" className="capitalize">date:</label>
+            <input type='date' name="date" value={formData.date} max={today} onChange={handleChange}  className="rounded-full px-4 py-2 flex items-center bg-gray-300 text-gray-400"></input>
           </div>
           
 
